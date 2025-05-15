7591b04e701aca9fd7afaadb89bef7ce47f1ce3b
CAndres438 <candresortiz123@gmail.com>
2025-05-15 18:21:18 -0500

    🔐 feat: implement JwtUtil for token generation and validation

--- src/main/java/com/caop/taskmanager_inform/security/JwtUtil.java
+++ src/main/java/com/caop/taskmanager_inform/security/JwtUtil.java
@@ -1,2 +1,60 @@
-package com.caop.taskmanager_inform.security;public class JwtUtil {
+package com.caop.taskmanager_inform.security;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtil.class);
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expirationMs;
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(String email) {
+        logger.info("Auth: Generating Token For User with email: {}", email);
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+    public String getEmailFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+            logger.info("Auth: Valid token");
+            return true;
+        } catch (JwtException e) {
+            logger.warn("Auth: Invalid token: {}", token);
+            return false;
+        }
+    }
 }
