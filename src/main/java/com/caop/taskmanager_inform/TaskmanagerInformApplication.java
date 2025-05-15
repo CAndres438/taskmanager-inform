0d99794e7d58ed434eafe7fe81611c1d33817cd6
CAndres438 <candresortiz123@gmail.com>
2025-05-15 12:39:30 -0500

    🎉 chore: initialize Spring Boot project with base dependencies and structure

---
+++ src/main/java/com/caop/taskmanager_inform/TaskmanagerInformApplication.java
@@ -0,0 +1,13 @@
+package com.caop.taskmanager_inform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerInformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerInformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caop/taskmanager_inform/TaskmanagerInformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caop.taskmanager_inform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerInformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
