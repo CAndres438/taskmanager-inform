6b0abb38d5bafa5c437a739bdcd48a27a50496fe
CAndres438 <candresortiz123@gmail.com>
2025-05-15 18:22:36 -0500

    ✨ feat: create Role entity

--- src/main/java/com/caop/taskmanager_inform/models/Role.java
+++ src/main/java/com/caop/taskmanager_inform/models/Role.java
@@ -1,2 +1,23 @@
-package com.caop.taskmanager_inform.models;public class Role {
+package com.caop.taskmanager_inform.models;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
