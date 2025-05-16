2a0ac4be7716461659a0af1375ac2bf5ee4ac435
CAndres438 <candresortiz123@gmail.com>
2025-05-15 21:56:17 -0500

    ✨ feat: complete the model Task with status and assigned user with consruct

--- src/main/java/com/caop/taskmanager_inform/models/Task.java
+++ src/main/java/com/caop/taskmanager_inform/models/Task.java
@@ -1,2 +1,87 @@
-package com.caop.taskmanager_inform.models;public class Task {
+package com.caop.taskmanager_inform.models;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status = TaskStatus.PENDING;
+
+    @ManyToOne
+    @JoinColumn(name = "assigned_user_id")
+    private User assignedTo;
+
+    public Task(){}
+
+    public Task(Integer id, String title, String description, LocalDateTime createdAt, TaskStatus status, User assignedTo) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.status = status;
+        this.assignedTo = assignedTo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public User getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(User assignedTo) {
+        this.assignedTo = assignedTo;
+    }
 }
