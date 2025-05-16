f3f61b7852fab8d40fc821a575f1dd08fefe80bf
CAndres438 <candresortiz123@gmail.com>
2025-05-15 21:04:57 -0500

    ✨ feat: add Role Repository and fix import problem role entity

--- src/main/java/com/caop/taskmanager_inform/repositories/RoleRepository.java
+++ src/main/java/com/caop/taskmanager_inform/repositories/RoleRepository.java
@@ -1,2 +1,11 @@
-package com.caop.taskmanager_inform.repositories;public interface RoleRepository {
+package com.caop.taskmanager_inform.repositories;
+
+import com.caop.taskmanager_inform.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
 }
