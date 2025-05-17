436083d26dc2b16c89b5f2a5eab7f879d55741b3
CAndres438 <candresortiz123@gmail.com>
2025-05-17 15:28:42 -0500

    ✨ feat: change order by in task get

--- src/main/java/com/caop/taskmanager_inform/controllers/TaskController.java
+++ src/main/java/com/caop/taskmanager_inform/controllers/TaskController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -37,7 +38,7 @@ public class TaskController {
             @RequestParam(required = false) String title,
             @RequestParam(required = false) String description,
             @RequestParam(required = false) String status,
-            @PageableDefault(size = 10, sort = "createdAt") Pageable pageable
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
     ) {
         logger.info("Task: Searching task with filters - title: {}, description: {}, status: {}", title, description, status);
         return ResponseEntity.ok(taskService.searchTasks(title, description, status, pageable));
